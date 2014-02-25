bb75d13ce200aca62bbfa2e97d0a113a18b30dfc
Artem <art7384@gmail.com>
2014-02-26 01:18:57 +0400

    startActivity

--- BookAgoo/src/com/itech/bookagoo/StartActivity.java
+++ BookAgoo/src/com/itech/bookagoo/StartActivity.java
@@ -1,5 +1,6 @@
 package com.itech.bookagoo;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 
 /**
@@ -7,4 +8,12 @@ import android.support.v7.app.ActionBarActivity;
  */
 public class StartActivity extends ActionBarActivity {
 
+    private String mMail = null;
+    private String mPass = null;
+
+    public class BaseFragment extends Fragment {
+
+        private
+
+    }
 }
