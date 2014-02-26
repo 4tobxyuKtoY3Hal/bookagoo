6e52819c61748adc02cd144defa6b0a99f5f6204
Artem <art7384@gmail.com>
2014-02-27 01:37:02 +0400

    v1.0.1.19

--- BookAgoo/src/com/itech/bookagoo/LoginFragment.java
+++ BookAgoo/src/com/itech/bookagoo/LoginFragment.java
@@ -1,9 +1,7 @@
 package com.itech.bookagoo;
 
-
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,24 +9,45 @@ import android.view.ViewGroup;
 /**
  * Created by Artem on 25.02.14.
  */
-public class LoginFragment extends Fragment {
+public class LoginFragment extends StartActivity.BaseFragment implements View.OnClickListener {
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_login, null);
+        View view = inflater.inflate(R.layout.fragment_login, null);
+
+        view.findViewById(R.id.fragmntLogin_Button_login).setOnClickListener(this);
+        view.findViewById(R.id.fragmntLogin_Button_createLogin).setOnClickListener(this);
+        view.findViewById(R.id.fragmntLogin_Button_loginFacebook).setOnClickListener(this);
+        view.findViewById(R.id.fragmntLogin_Button_loginTwitter).setOnClickListener(this);
+
+
+        return view;
+
     }
 
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
 
-        public class BaseFragment extends Fragment {
-            
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.fragmntLogin_Button_login:
+                Log.d("log_test", "login");
+                break;
+            case R.id.fragmntLogin_Button_createLogin:
+                Log.d("log_test", "createLogin");
+                _activity.gotoFragment(0);
+                break;
+            case R.id.fragmntLogin_Button_loginFacebook:
+                Log.d("log_test", "loginFacebook");
+                break;
+            case R.id.fragmntLogin_Button_loginTwitter:
+                Log.d("log_test", "loginTwitter");
+                break;
         }
+    }
+
 
 }
--- BookAgoo/src/com/itech/bookagoo/StartActivity.java
+++ BookAgoo/src/com/itech/bookagoo/StartActivity.java
@@ -1,19 +1,91 @@
 package com.itech.bookagoo;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 /**
  * Created by Artem on 25.02.14.
  */
+
 public class StartActivity extends ActionBarActivity {
 
-    private String mMail = null;
-    private String mPass = null;
+    private static StartActivity mStartActivity;
+    private BaseFragment mFragment = null;
+    private static LoginFragment mLoginFragment = new LoginFragment();
+    private static CreateAccountFragment mCreateAccountFragment = new CreateAccountFragment();
 
-    public class BaseFragment extends Fragment {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-        private
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        mStartActivity = this;
+
+  //      gotoFragment(mLoginFragment);
+        gotoFragment(mCreateAccountFragment);
 
     }
+
+    private void gotoFragment(BaseFragment fragment) {
+
+        Log.d("log_test", "gotoFragment>" + fragment);
+
+        ActionBar actionBar = getSupportActionBar();
+        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+
+        if (mFragment != null) {
+            trans.remove(mFragment);
+            trans.commit();
+        }
+
+        mFragment = fragment;
+
+        int id = fragment.getIdTitle();
+        if (id > 0) {
+            actionBar.show();
+            actionBar.setTitle(id);
+        } else {
+            actionBar.hide();
+        }
+        Log.d("log_test", "v");
+        trans.add(R.id.activityStart_layout_container, mFragment).commit();
+        Log.d("log_test", "^");
+    }
+
+    public static void gotoFragment(int iFragment) {
+
+        switch (iFragment) {
+            case Fragments.CREATE_ACCOUNT:
+                mStartActivity.gotoFragment(mCreateAccountFragment);
+                break;
+            case Fragments.LOGIN:
+                mStartActivity.gotoFragment(mLoginFragment);
+                break;
+        }
+    }
+
+    public static class BaseFragment extends Fragment {
+
+        protected StartActivity _activity = null;
+
+        public BaseFragment() {
+            _activity = (StartActivity) getActivity();
+        }
+
+        public int getIdTitle() {
+            return -1;
+        }
+
+    }
+
+    public class Fragments {
+        public static final int LOGIN = 0;
+        public static final int CREATE_ACCOUNT = 1;
+    }
+
 }
